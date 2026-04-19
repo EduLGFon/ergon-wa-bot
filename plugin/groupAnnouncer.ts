c23020b01bead62bcd6e50fde1d637fd346238db
EduLGFon <edulgfon@gmail.com>
2026-04-19 13:32:33 +0000

    fix announcer logic error

--- plugin/groupAnnouncer.ts
+++ plugin/groupAnnouncer.ts
@@ -2,8 +2,7 @@ import { getMedia, reactToMsg, sendMsg } from '../util/messages.js'
 import { randomDelay } from '../util/functions.js'
 import { Group, Msg, User } from '../map.js'
 import { AnyMessageContent } from 'baileys'
-import { getUser } from './prisma.js'
-type Announcement = { text?: str; caption?: str; chats: str[]; tag?: str }
+type Announcement = { text?: str; caption?: str; groups?: str[]; tag?: str; msg?: Msg }
 // Announcement = simple text msg or media msg (replace text by caption)
 
 /** How does this shit work?
@@ -28,44 +27,48 @@ const allowedTags = {
 }
 allowedTags['#todos'] = [...allowedTags['#diurno'], ...allowedTags['#noturno']]
 
+export { allowedTags }
 export default checkGroupAnnouncer
 async function checkGroupAnnouncer(msg: Msg, user: User, group?: Group) {
 	if (!group || !allowedTags['#todos'].includes(group.id) || msg.isBot) return
 	// ignore msgs from DMs, from other groups or that was sent by the bot
 
-	let announceMsg: Announcement = { chats: [] }
+	let announceMsg: Announcement = {}
 	const lowText = msg.text.toLowerCase()
 	for (const [key, value] of Object.entries(allowedTags)) {
 		if (lowText.includes(key))
 			announceMsg = {
 				tag: key, // tag trigger
-				chats: value.filter(g => g !== group.id),
+				groups: value.filter(g => g !== group.id),
 				// all groups that wasn't the group the msg was sent
 			}
 	}
 
 	// ignore msgs that not contain any tag
-	if (!announceMsg.tag || (lowText === announceMsg.tag && !msg.media)) {
+	if (!announceMsg.tag) return
+	// ignore msgs equals to just '#todos'
+	if (lowText === announceMsg.tag && !msg.media && !msg.quoted?.media) {
 		randomDelay(500, 1_500).then(() => reactToMsg.bind(msg)('question'))
 		// ignore no empty content msgs (only tag msgs)
 		return
 	}
 
-	announceMsg.text = `[${announceMsg.tag.toUpperCase()}] *${user.name}:*\n${msg.text}`
+	announceMsg.text = `*${user.name}:* ${msg.text}`
 	if (msg.quoted) {
-		const quoteAuthor = await getUser({ id: msg.quoted.author })
-		announceMsg.text = `> *${quoteAuthor?.name || 'usuário'}:* ${msg.quoted.text}\n${announceMsg.text}`
+		announceMsg.text = `> ${msg.quoted.text}\n${announceMsg.text}`
 	}
 
-	if (msg.media) {
+	if (msg.media || msg.quoted?.media) {
 		const media = await getMedia(msg)
+		const type = msg.media ? msg.type : msg.quoted?.type!
 		announceMsg = {
 			caption: announceMsg.text,
-			[msg.type]: media!.buffer,
-			chats: announceMsg.chats,
+			[type]: media!.buffer,
+			groups: announceMsg.groups,
 		}
 	}
 
+	announceMsg.msg = msg
 	msgQueue.push(announceMsg)
 	// push the announcement to the end of the queue
 
@@ -78,9 +81,12 @@ async function sendAnnouncements() {
 	// if (isSending === true) return // containment measure to stop the hell
 	isSending = true // now, sendAnnouncements() won't be called again
 	// until the queue is empty
-	randomDelay().then(() => reactToMsg.bind(msgQueue[0] as any)('check'))
 
-	for (const g of msgQueue[0].chats) {
+	const msg = msgQueue[0].msg!
+	// it is important bc msgQueue[0] will be deleted soon
+	randomDelay().then(() => reactToMsg.bind(msg)('ok'))
+
+	for (const g of msgQueue[0].groups!) {
 		await randomDelay(1_000, 2_500)
 		// a random delay is required to Meta not flag us as
 		// a spam bot. random delays and different msgs looks
