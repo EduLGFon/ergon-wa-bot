f144394c071e2fe75ceabccb357a0b01fbea5123
EduLGFon <edulgfon@gmail.com>
2026-04-19 18:09:17 +0000

    update pm2 config

--- conf/bun.config.cjs
+++ conf/bun.config.cjs
@@ -3,21 +3,24 @@
  * See pm2 documentation for more info:
  * https://pm2.keymetrics.io/docs/usage/application-declaration/
  */
-const node_args = [
+const runtime_args = [
 	'--expose-gc',
 	// '--max-old-space-size=16384',
 	'--env-file=conf/.env',
+	'--hot'
 ]
 
 module.exports = { // yea, i really need to use module.exports. don't rage!
 	apps: [{ // pm2 launch settings
 		name: 'wa',
 		script: 'wa.ts', /// main file
-		node_args,
+		interpreter: 'bun',
+		interpreter_args: runtime_args,
 		env: {
-        	NODE_EXTRA_CA_CERTS: 'conf/smufeschain.pem',
+			NODE_EXTRA_CA_CERTS: 'conf/smufeschain.pem',
         },
 		log_file: 'conf/gen/out.log',
 		merge_logs: true,
+
 	}],
 }
