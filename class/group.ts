b233d12e9bd9d580ab3267d756d0b73a0eb38601
EduLGFon <edulgfon@gmail.com>
2026-07-11 23:26:37 -0300

    Remove commented code from class/group.ts
    - Remove commented out indexParticipants method
    - Remove commented code in constructor and checkData
    - Remove commented ephemeral property

--- class/group.ts
+++ class/group.ts
@@ -12,7 +12,6 @@ export default class Group {
 	announce?: bool // is set when group only allows admins to write msgs
 	members: GroupParticipant[]
 	size: num // group members size
-	// ephemeral?: num;
 	invite?: str // invite link
 	author?: str // the person who added you (this property name really sucks)
 	msgs: Collection<str, Msg> // cached msgs
@@ -21,18 +20,14 @@ export default class Group {
 		this.id = data.id
 		// @ts-ignore Shut up TypeScript
 		this.name = data.subject || data.name
-		// this.owner = g.owner // it's commented bc I don't want to fill cache with it
-		// (I don't use it)
 		//@ts-ignore
 		this.nameTimestamp = data?.subjectTime || data?.nameTimestamp
 		this.creation = data.creation
-		// this.desc = g.desc
 		this.restrict = data.restrict
 		this.announce = data.announce
 		// @ts-ignore
 		this.members = data?.participants || data?.members
 		this.size = data.size || this.members.length
-		// this.ephemeral = data.ephemeral || data.ephemeralDuration;
 		// @ts-ignore
 		this.invite = data.inviteCode || data.invite
 		this.author = data.author
@@ -76,37 +71,7 @@ export default class Group {
 		return msgs
 	}
 
-	// async indexParticipants(data: Group | GroupMetadata, bot: Baileys) {
-	// 	let participants: { phone: str; admin: any }[] = []
-	// 	const members: User[] = []
-
-	// 	if ((data as Group).members) { // data is a cached group
-	// 		data = data as Group
-	// 		participants = data.members.map((m) => {
-	// 			return { phone: m.phone, admin: m.admin }
-	// 		})
-	// 	} else { // data is a raw group metadata
-	// 		data = data as GroupMetadata
-	// 		participants = data.participants.map((p) => {
-	// 			return { phone: p.id, admin: p.admin }
-	// 		})
-	// 	}
-
-	// 	for (const m of participants) {
-	// 		const user = await getUser({ phone: m.phone })
-	// 		user.admin = m.admin
-	// 		members.push(user)
-	// 	}
-
-	// 	return members
-	// }
-
 	async checkData() {
-		// this.members = await this.indexParticipants(this.members as any, bot)
-		// I don't save any data of groups, but I let this func here bc
-		// if some day I store groups data,
-		// the code will be prepared to check these data
-
 		return this
 	}
 }
